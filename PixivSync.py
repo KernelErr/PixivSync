3f81dd3f5cc662ebfc2cff0e29b6c37a44626c33
ChinaKevinLi <hackeverything@qq.com>
2019-01-01 11:48:55 +0800

    Real initial commit

---
+++ PixivSync.py
@@ -0,0 +1,205 @@
+# -*- coding: utf-8 -*
+from pixivpy3 import AppPixivAPI
+import ConfigParser
+import os
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+import time
+
+def get_config():
+    print("尝试读取配置...")
+    configfile = ConfigParser.ConfigParser()
+    path = os.path.split(os.path.realpath(__file__)[0] + '/config.ini')
+    configfile.read(path)
+    config=[]    
+    try:
+        config.append(configfile.get('my','username'))
+        config.append(configfile.get('my','password'))
+        config.append(configfile.get('following','id'))
+        print("配置读取成功。")
+    except:
+        exit("检查你的配置文件是否正常。")
+    else:
+        return config
+
+def del_same(seta, setb):
+    temp = []
+    tempa = []
+    tempb = []
+    for eacha in seta:
+        tempa.append(eacha.strip('\r').strip('\n'))
+    for eachb in setb:
+        tempb.append(eachb.strip('\r').strip('\n'))
+    for eachc in tempb:
+        temp.append(eachc)
+    for eachd in tempb:
+        for eache in tempa:
+            if eachd == eache:
+                temp.remove(eachd)
+    return temp
+
+def get_user(aapi, uid):
+    json_result = aapi.user_illusts(uid)
+    urls = []
+    while 1:
+        try:
+            nLen = len(json_result.illusts)
+        except Exception as e:
+            print(e)
+
+        for illust in json_result.illusts:
+            if illust.page_count == 1 and illust.type != 'ugoira':
+                urls.append(illust.meta_single_page.original_image_url)
+            elif illust.page_count > 1:
+                image_urls = [
+                    page.image_urls.original
+                    for page in illust.meta_pages
+                    ]
+                for url in image_urls:
+                    urls.append(url)
+            else:
+                image_urls = []
+
+        try:
+            next_qs = aapi.parse_qs(json_result.next_url)
+            if next_qs is None:
+                break
+            json_result = aapi.user_illusts(**next_qs)
+        except Exception as e:
+            print(e)
+            break
+
+    return urls
+    
+def get_bookmarks(aapi, rule):
+    json_result = aapi.user_bookmarks_illust(aapi.user_id,restrict=rule)
+    urls = []
+    while 1:
+        try:
+            nLen = len(json_result.illusts)
+        except Exception as e:
+            print(e)
+
+        for illust in json_result.illusts:
+            if illust.page_count == 1 and illust.type != 'ugoira':
+                urls.append(illust.meta_single_page.original_image_url)
+            elif illust.page_count > 1:
+                image_urls = [
+                    page.image_urls.original
+                    for page in illust.meta_pages
+                    ]
+                for url in image_urls:
+                    urls.append(url)
+            else:
+                image_urls = []
+
+        try:
+            next_qs = aapi.parse_qs(json_result.next_url)
+            if next_qs is None:
+                break
+            json_result = aapi.user_bookmarks_illust(**next_qs)
+        except Exception as e:
+            print(e)
+            break
+
+    filename = './outputs/bookmarks_' + rule
+    if not os.path.exists(filename + '.txt'):
+       open(filename + '.txt','w+')
+    with open(filename + '.txt','r+') as f:
+       old = f.readlines()
+       now = urls
+       new = del_same(old, now)
+       with open(filename + '.txt','w+') as fa:
+           for url in now:
+               fa.write(str(url) + '\n')
+       with open(filename + '_new.txt','w+') as fb:
+           for url in new:
+               fb.write(str(url) + '\n')
+    print("更新收藏夹成功。")
+
+def get_daily_rank(aapi):
+    json_result = aapi.illust_ranking(mode='day', date=None)
+    json_result = json_result.illusts
+    ids = []
+    urls = []
+    for number in range(0,9):
+        ids.append(json_result[number].id)
+    for each in ids:
+        illust = aapi.illust_detail(each)
+        illust = illust.illust
+        if illust.page_count == 1 and illust.type != 'ugoira':
+            urls.append(illust.meta_single_page.original_image_url)
+        elif illust.page_count > 1:
+            image_urls = [
+                    page.image_urls.original
+                    for page in illust.meta_pages
+                    ]
+            for url in image_urls:
+                urls.append(url)
+    filename = './outputs/' + str(time.strftime("%Y%m%d",time.localtime())) + ".txt"
+    with open(filename,'w+') as f:
+        for url in urls:
+            f.write(str(url) + '\n')
+    print("获取每日Top 10成功。")
+
+def get_following(aapi,following):
+    for user in following:
+        userinfo = aapi.user_detail(user)
+        username = userinfo.user.name
+        filename = './outputs/' + str(username)
+        urls = get_user(aapi,user)
+        if not os.path.exists(filename + '.txt'):
+            with open(filename + '.txt','w+') as f:
+                f.write('')
+        with open(filename + '.txt','r+') as f:
+           old = f.readlines()
+           now = get_user(aapi,user)
+           new = del_same(old, now)
+           with open(filename + '.txt','w+') as fa:
+               for url in now:
+                   fa.write(str(url) + '\n')
+           with open(filename + '_new.txt','w+') as fb:
+               for url in new:
+                     fb.write(str(url) + '\n')
+        print("更新画师 " + str(username) + " 的作品成功。")
+
+def main():
+    print("PixivSync 由 ChinaKevinLi 制作")
+    print("Github地址：https://github.com/ChinaKevinLi/PixivSync")
+    print("交流QQ群：761049620")
+    try:
+        test = sys.argv[1]
+    except:
+        exit("无命令，请查询文档。")
+    aapi = AppPixivAPI()
+    config = get_config()
+    username = config[0]
+    password = config[1]
+    following = []
+    for id in config[2].split(','):
+        following.append(id)
+    print("登录帐号中...")
+    try:
+        userinfo = aapi.login(username,password)
+    except:
+        exit("登录失败。")
+    else:
+        print("登录成功。欢迎您，" + str(userinfo.response.user.name) + "。")
+    if sys.argv[1] == "dailyrank":
+        print("开始获取每日Top 10，请耐心等待。")
+        get_daily_rank(aapi)
+    elif sys.argv[1] == "following":
+        print("开始更新关注者的作品，请耐心等待。")
+        get_following(aapi,following)
+    elif sys.argv[1] == "bookmarks":
+        print("开始更新公开收藏列表，请耐心等待。")
+        get_bookmarks(aapi, 'public')       
+    elif sys.argv[1] == "pribookmarks":
+        print("开始更新私人收藏列表，请耐心等待。")
+        urls = get_bookmarks(aapi, 'private')
+    else:
+        exit("无对应命令，请查询文档。")
+
+if __name__ == '__main__':
+    main()
--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # PixivSync
-Pixiv图片下载及同步工具
+一个Pixiv同步工具，可以获取Pixiv的原图地址。具体的特性如下：
+- 支持获取每日Top 10
+- 单独列出关注者的新作品，方便增量下载
+- 单独列出收藏夹（公开与私人分离）的新作品，方便增量下载
+
+## 依赖
+程序在Python 2下成功运行，如果你更喜欢Python 3，那么也应该可以兼容。但在使用前请安装pixivpy包，通过pip的简便安装命令如下：
+```
+pip install pixivpy
+```
+之后你需要编辑config.ini文件，在username和password的等号后直接填入自己的用户名和密码，在following的id的等号后填入你喜欢的画师id号码，多个id用英文逗号分隔，一个标准的示例如下：
+```
+[my]
+username=a@a.com
+password=123456
+
+[following]
+id=1,2,3
+```
+
+## 用法
+请在程序目录下运行本程序，所有输出结果会保存在outputs文件夹中。
+```
+python2 PixivSync.py dailyrank #获取每日Top 10
+python2 PixivSync.py bookmarks #获取公开收藏夹
+python2 PixivSync.py pribookmarks #获取私人收藏夹
+python2 PixivSync.py following #获取关注画师的作品
+```
+程序的运行速度直接依赖于你连接Pixiv服务器的速度，如果无法连接Pixiv，那么程序也无法运行。
+
+## 提示
+本程序是一个Python脚本，需要一定基础。并且Pixiv对于直接访问图片地址的行为有防盗链措施，需要一些绕过操作。
+
+## 相关程序
+有很多优秀的便于使用的类似程序，一一列举：
+- [PixivUserBatchDownload](https://github.com/Mapaler/PixivUserBatchDownload)
+- [XZPixivDownloader](https://github.com/xuejianxianzun/XZPixivDownloader)
